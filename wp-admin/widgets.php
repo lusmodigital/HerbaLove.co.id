de6377c7f7d236295d6f30f50c72d6d0d36a9227
Aisyah Quinta Evelina <aisyahevelinaa@gmail.com>
2021-11-19 08:09:58 +0700

    upload website

---
+++ wp-admin/widgets.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://herbalove.co.id/wp-login.php?redirect_to=https%3A%2F%2Fherbalove.co.id%2Fwp-admin%2Fwidgets.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://herbalove.co.id/wp-login.php?redirect_to=https%3A%2F%2Fherbalove.co.id%2Fwp-admin%2Fwidgets.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://herbalove.co.id/wp-login.php?redirect_to=https%3A%2F%2Fherbalove.co.id%2Fwp-admin%2Fwidgets.php&reauth=1">https://herbalove.co.id/wp-login.php?redirect_to=https%3A%2F%2Fherbalove.co.id%2Fwp-admin%2Fwidgets.php&reauth=1</a></p>
+	</body>
+</html>
